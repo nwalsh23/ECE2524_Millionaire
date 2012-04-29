166842d3c6313a9a72d3d60f5ce7b16a12f3c946
gomezfx <gomezfx@gmail.com>
2012-04-29 18:58:42 -0400

    Fleshed out some more functions

--- millionaire.cpp
+++ millionaire.cpp
@@ -1,16 +1,23 @@
 #include "millionaire.h"
 #include <string>
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
 millionaire::millionaire()
 {
+	score = 5;
+	chances = 1;
+	win = false;
 }
 
 void millionaire::play(void)
 {
 	welcome();
-
+	question_read();
+	cout << "DONE" << endl;
+	while(1);
 	return;
 }
 
@@ -26,7 +33,7 @@ void millionaire::welcome(void)
 		getline(std::cin, response, '\n' );
 		if (response == "yes")
 			{
-				cout << "Let's begin!" << endl;
+				cout << "Let's begin!" << endl << endl;
 				return;
 			}
 		else if (response == "no")
@@ -37,4 +44,61 @@ void millionaire::welcome(void)
 			}
 		else cout << "You must type yes or no" << endl;
 	}
+}
+
+void millionaire::question_read(void)
+{
+	std::string response;
+	ifstream questions ("Flowers_Questions");
+	string question, A, B, C, D, answer;
+
+
+	if (questions.is_open())
+		{
+			while ( questions.good() )
+				{
+					
+					getline (questions, question);
+					cout << question << endl;
+					getline (questions, A);
+					cout << A << endl;
+					getline (questions, B);
+					cout << B << endl;
+					getline (questions, C);
+					cout << C << endl;
+					getline (questions, D);
+					cout << D << endl;
+					
+
+					cout << endl;
+					cout << "Enter an answer or type Options to get some help" << endl;
+					getline(std::cin, response, '\n' );
+					getline (questions, answer);
+					check_answer(answer, response);
+					
+				}
+			questions.close();
+		}
+
+	return;
+}
+
+void millionaire::check_answer(string answer, string response)
+{
+	if (answer == response)
+		{
+			cout << "You got it right!" << endl;
+			score = score*2;
+			cout << "You total sum is $" << score << endl;
+			cout << "Press Enter to proceed to the next question" << endl;
+			cin.get();
+		}	
+	else
+		{
+			cout << "You got it wrong!" << endl;
+			cout << "Press Enter to proceed to the next question" << endl;
+			cin.get();
+		}
+
+	return;
 }
--- millionaire.h
+++ millionaire.h
@@ -1,4 +1,6 @@
+#include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -14,7 +16,7 @@ class millionaire
 		void play(void);
 		void welcome(void);
 		void question_read(void);
-		char check_answer(char answer);
+		void check_answer(string answer, string response);
 		void adjust_score(void);
 		void check_chances(void);
 		void final_screen(void);

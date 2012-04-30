5f9808b9e0f2a18eba01fa41965b325f82ac46c1
gomezfx <gomezfx@gmail.com>
2012-04-30 01:04:14 -0400

    Added a scoring system

--- millionaire.cpp
+++ millionaire.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 millionaire::millionaire()
 {
-	score = 5;
+	round = 1;
+	score = 0;
 	chances = 1;
 	win = false;
 }
@@ -74,7 +75,13 @@ void millionaire::question_read(void)
 					cout << "Enter an answer or type Options to get some help" << endl;
 					getline(std::cin, response, '\n' );
 					getline (questions, answer);
-					check_answer(answer, response);
+					if (check_answer(answer, response)) adjust_score();
+					if (round == 16) exit(0);
+					else
+						{
+							cout << "Press Enter to proceed to the next question" << endl;
+							cin.get();
+						}
 					
 				}
 			questions.close();
@@ -83,22 +90,77 @@ void millionaire::question_read(void)
 	return;
 }
 
-void millionaire::check_answer(string answer, string response)
+bool millionaire::check_answer(string answer, string response)
 {
 	if (answer == response)
 		{
 			cout << "You got it right!" << endl;
-			score = score*2;
-			cout << "You total sum is $" << score << endl;
-			cout << "Press Enter to proceed to the next question" << endl;
-			cin.get();
+
+			return true;
 		}	
 	else
 		{
 			cout << "You got it wrong!" << endl;
-			cout << "Press Enter to proceed to the next question" << endl;
-			cin.get();
+
+			return false;
+		}
+}
+
+void millionaire::adjust_score(void)
+{
+	switch (round)
+		{
+			case 1:
+				score = 100;
+				break;
+			case 2:
+				score = 200;
+				break;
+			case 3:
+				score = 300;
+				break;
+			case 4:
+				score = 500;
+				break;
+			case 5:
+				score = 1000;
+				break;
+			case 6:
+				score = 2000;
+				break;
+			case 7:
+				score = 4000;
+				break;
+			case 8:
+				score = 8000;
+				break;
+			case 9:
+				score = 16000;
+				break;
+			case 10:
+				score = 32000;
+				break;
+			case 11:
+				score = 64000;
+				break;
+			case 12:
+				score = 125000;
+				break;
+			case 13:
+				score = 250000;
+				break;
+			case 14:
+				score = 500000;
+				break;
+			case 15:
+				score = 1000000;
+				break;
+			default:
+				
+				break;
 		}
 
+	round++;
+	cout << "You total sum is $" << score << endl;
 	return;
 }
--- millionaire.h
+++ millionaire.h
@@ -7,6 +7,7 @@ using namespace std;
 class millionaire
 {
 	public:
+		int round;
 		int score;
 		int chances;
 		bool win;
@@ -16,7 +17,7 @@ class millionaire
 		void play(void);
 		void welcome(void);
 		void question_read(void);
-		void check_answer(string answer, string response);
+		bool check_answer(string answer, string response);
 		void adjust_score(void);
 		void check_chances(void);
 		void final_screen(void);
